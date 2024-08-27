41c1bb5e93e113fcca68567057c1c5fb8ca09e8a
Sena2K <senamurilo2@gmail.com>
2024-08-27 20:20:47 -0300

    romarinho bolado

--- main2.py
+++ main2.py
@@ -8,11 +8,16 @@ image_path = None
 image_metadata = {}
 previous_state = None
 
-
-def resize_image(img):
-    img = img.resize((800, 600), Image.Resampling.LANCZOS)
+def resize_image(img, new_width=None, new_height=None, keep_aspect_ratio=True):
+    largura, altura = img.size
+    if keep_aspect_ratio:
+        aspect_ratio = largura / altura
+        if new_width:
+            new_height = int(new_width / aspect_ratio)
+        elif new_height:
+            new_width = int(new_height * aspect_ratio)
+    img = img.resize((new_width, new_height), Image.Resampling.LANCZOS)
     return img
-
 
 def open_image(filename):
     global image_atual
@@ -21,9 +26,8 @@ def open_image(filename):
     global previous_state
     image_path = filename
     image_atual = Image.open(filename)
-    previous_state = image_atual.copy()  # Save the original state
+    previous_state = image_atual.copy()
 
-    # Extracting important metadata
     if image_atual._getexif():
         for k, v in image_atual._getexif().items():
             tag_name = ExifTags.TAGS.get(k)
@@ -37,11 +41,11 @@ def open_image(filename):
                 else:
                     image_metadata[tag_name] = v
 
-    resized_img = resize_image(image_atual)
     img_bytes = io.BytesIO()
-    resized_img.save(img_bytes, format='PNG')
+    image_atual.save(img_bytes, format='PNG')
     window['-IMAGE-'].update(data=img_bytes.getvalue())
-
+    window['-WIDTH-'].update(value=str(image_atual.size[0]))
+    window['-HEIGHT-'].update(value=str(image_atual.size[1]))
 
 def save_image(filename):
     global image_atual
@@ -49,14 +53,12 @@ def save_image(filename):
         with open(filename, 'wb') as file:
             image_atual.save(file)
 
-
 def get_gps_decimal(gps_info, ref):
     d, m, s = gps_info
     decimal = d + (m / 60.0) + (s / 3600.0)
     if ref in ['S', 'W']:
         decimal = -decimal
     return decimal
-
 
 def info_image():
     global image_atual
@@ -68,12 +70,10 @@ def info_image():
         tamanho_bytes = os.path.getsize(image_path)
         tamanho_mb = tamanho_bytes / (1024 * 1024)
 
-        # Displaying only relevant metadata
         model = image_metadata.get('Model', 'N/A')
         make = image_metadata.get('Make', 'N/A')
         date_time = image_metadata.get('DateTime', 'N/A')
 
-        # GPS Information
         gps_info = image_metadata.get('GPSInfo', {})
         if gps_info:
             lat = get_gps_decimal(gps_info.get('GPSLatitude', (0, 0, 0)), gps_info.get('GPSLatitudeRef', 'N'))
@@ -86,14 +86,13 @@ def info_image():
                  f"Modelo da Câmera: {model}\nFabricante: {make}\nData/Hora: {date_time}\n"
                  f"Latitude: {lat}\nLongitude: {lon}")
 
-
 def apply_gray_filter():
     global image_atual
     global previous_state
     if image_atual:
         largura, altura = image_atual.size
         pixels = image_atual.load()
-        previous_state = image_atual.copy()  # Save the state before applying the filter
+        previous_state = image_atual.copy()
 
         for w in range(largura):
             for h in range(altura):
@@ -101,13 +100,21 @@ def apply_gray_filter():
                 gray = int(0.1 * r + 0.1 * g + 0.8 * b)
                 pixels[w, h] = (gray, gray, gray)
 
-        resized_img = resize_image(image_atual)
+        img_bytes = io.BytesIO()
+        image_atual.save(img_bytes, format='PNG')
+        window['-IMAGE-'].update(data=img_bytes.getvalue())
+
+def resize_image_and_show(new_width=None, new_height=None, keep_aspect_ratio=True):
+    global image_atual
+    if image_atual:
+        resized_img = resize_image(image_atual, new_width=new_width, new_height=new_height, keep_aspect_ratio=keep_aspect_ratio)
         img_bytes = io.BytesIO()
         resized_img.save(img_bytes, format='PNG')
         window['-IMAGE-'].update(data=img_bytes.getvalue())
+        window['-WIDTH-'].update(value=str(resized_img.size[0]))
+        window['-HEIGHT-'].update(value=str(resized_img.size[1]))
     else:
         sg.popup("Nenhuma imagem aberta")
-
 
 layout = [
     [sg.Menu([
@@ -116,6 +123,10 @@ layout = [
         ['Sobre', ['Desenvolvedor']]
     ])],
     [sg.Image(key='-IMAGE-', size=(800, 600))],
+    [sg.Text('Largura:'), sg.InputText(key='-WIDTH-', size=(10, 1), enable_events=True),
+     sg.Text('Altura:'), sg.InputText(key='-HEIGHT-', size=(10, 1), enable_events=True)],
+    [sg.Checkbox('Manter Proporção', default=True, key='-ASPECT_RATIO-')],
+    [sg.Button('Redimensionar'), sg.Button('Sair')]
 ]
 
 window = sg.Window('Aplicativo de Imagem', layout, finalize=True)
@@ -123,7 +134,7 @@ window = sg.Window('Aplicativo de Imagem', layout, finalize=True)
 while True:
     event, values = window.read()
 
-    if event in (sg.WINDOW_CLOSED, 'Fechar'):
+    if event in (sg.WINDOW_CLOSED, 'Sair'):
         break
     elif event == 'Abrir':
         arquivo = sg.popup_get_file('Selecionar imagem', file_types=(("Imagens", "*.png;*.jpg;*.jpeg;*.gif"),))
@@ -139,6 +150,19 @@ while True:
         info_image()
     elif event == 'Aplicar Filtro de Cinza':
         apply_gray_filter()
+    elif event == 'Redimensionar':
+        new_width = int(values['-WIDTH-']) if values['-WIDTH-'] else None
+        new_height = int(values['-HEIGHT-']) if values['-HEIGHT-'] else None
+        keep_aspect_ratio = values['-ASPECT_RATIO-']
+        resize_image_and_show(new_width=new_width, new_height=new_height, keep_aspect_ratio=keep_aspect_ratio)
+    elif event == '-WIDTH-' and values['-WIDTH-'] and values['-ASPECT_RATIO-']:
+        largura_atual = int(values['-WIDTH-'])
+        altura_atual = int(image_atual.size[1] * largura_atual / image_atual.size[0])
+        window['-HEIGHT-'].update(value=str(altura_atual))
+    elif event == '-HEIGHT-' and values['-HEIGHT-'] and values['-ASPECT_RATIO-']:
+        altura_atual = int(values['-HEIGHT-'])
+        largura_atual = int(image_atual.size[0] * altura_atual / image_atual.size[1])
+        window['-WIDTH-'].update(value=str(largura_atual))
     elif event == 'Desenvolvedor':
         sg.popup('Desenvolvido por [Seu Nome] - BCC 6º Semestre')
 
